bd2d7ff0d6deca13045f7bbddac95238df43e169
Luciano Gallo <102675296+luchogallo92@users.noreply.github.com>
2024-02-09 10:33:11 -0300

    1.3

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -1,6 +1,28 @@
 
 
 
+///llamado a skills
+// const skillbar = document.getElementById ("text-and-bars")
+
+// let skillsHTML = skills.map(skill => {
+//     return `
+//         <div id="text-and-bars">
+//             <h4>Herramientas gráficas</h4>
+//             <span>${skill.id}</span>
+//             <div id="progress-bar-background">
+//                 <div id="progress-bar-colored">
+//                     <div id="progress-span">
+//                         <span>${skill.progress}</span>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     `;
+// });
+
+// skillsContainer.innerHTML = skillsHTML.join('');
+
+
 function showContent() {
     element = document.getElementById("menu-header");
     check = document.getElementById("menu-nav");
@@ -13,3 +35,9 @@ function showContent() {
 }
 
 AOS.init();
+
+// renderSkills(skills);
+
+
+
+
---
+++ Assets/JS/skills.js
@@ -0,0 +1,18 @@
+
+
+let skills = [
+    { 
+        id: "Rhinceros",
+        progress: "90%"
+    },
+
+    { 
+        id: "Keyshot",
+        progress: "70%"
+    },
+
+    { 
+        id: "Web Design",
+        progress: "85%"
+    }
+];
